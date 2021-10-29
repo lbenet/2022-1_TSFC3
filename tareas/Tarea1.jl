8286623fd1d00d62e1da56fbab5eb8e839353e3d
Luis Benet <lbenet@users.noreply.github.com>
2021-10-28 20:01:09 -0500

    Tarea 1 (#9)
    * Agrego paqueterías a Project.toml y Manifest.toml
    
    * Agrego Tarea1.jl y test1_nr.jl

---
+++ tareas/Tarea1.jl
@@ -0,0 +1,46 @@
+# # Tarea 1
+
+#-
+# > **Fecha de entrega** (aceptación del *Pull Request*): Jueves 11 Noviembre
+#
+
+#-
+# > **NOTA:** Esta tarea requiere que suban código (el archivo `intervalos.jl`)
+# > *únicamente*, que deberá estar en un subdirectorio de `tareas/`. Ese archivo
+# > debe estar adecuadamente documentado o, al menos, comentado.
+#
+
+#-
+# ### Ejercicio 1: Módulo `Intervalos`
+#
+# - Definan la estructura `Intervalo`, que debe funcionar para `Float64` y `BigFoat`. El uso de
+#     `Intervalo(a)` debe ser posible y equivalente a `Intervalo(a,a)`. Definan qué hacer si el `Intervalo`
+#     no está correctamente definido (por ejemplo, el ínfimo es mayor que el supremo).
+#
+# - Definan la función `intervalo_vacio`, que devuelve al `Intervalo` que no tiene elementos; piensen cómo
+#     definir el intervalo vacío y qué hacer para que su estructura permita definir a este intervalo especial.
+#
+# - Extiendan las relaciones de conjuntos `==`, `issubset` (`⊆`), `isinterior` (`⪽`), `in` (`∈`),
+#     `hull` (`⊔`), `intersect` (`∩`) para usarlas con intervalos. Noten que los símbolos `⪽` y `⊔`
+#     no existen en Julia y los usaremos. La función `union` (`∪`) háganla sinónimo de `hull`.
+#
+# - Extiendan las funciones aritméticas (`+`, `-`, `*`, `/`); por ahora no nos ocuparemos del redondeo. Sin embargo,
+#     asegúrense que las operaciones, cuando tenga sentido, pueden involucrar números enteros, `Float64` o `BigFoat`,
+#     entre otros, o sea, `<:Real`, o incluso sólo un intervalo.
+#
+# - Extiendan las potencias enteras para intervalos; consideren por ahora sólo potencias enteras.
+#
+# - El código escrito debe estar dentro de un *módulo* de Julia llamado `Intervalos`, y éste deberá incluirse en el
+#     archivo `intervalos.jl`, que contiene todas las definiciones anteriores.
+#
+
+#-
+# ### Ejercicio 2: Tests
+#
+# Asegúrense que lo desarrollado en el Ejercicio 1 *pase* las pruebas que se encuentran en el archivo
+# `tareas/Tests/test1_nr.jl`. Para verificar esto, lo que deben hacer es (desde el REPL)
+# ```julia
+#     include("tareas/SuDirTarea1/intervalos.jl")
+#     include("tareas/Tests/test1_nr.jl")
+# ```
+# y esto debe **no** arrojar ningún error. (Eventualmente automatizaremos este tipo de pruebas.)
---
+++ tareas/Tests/test1_nr.jl
@@ -0,0 +1,90 @@
+# Tests de la Tarea1 (`intervalos.jl`), sin usar redondeo.
+
+using Test
+
+include("../intervalos.jl")
+
+using .Intervalos
+
+u = Intervalo(1.0)
+z = Intervalo(0.0)
+a = Intervalo(1.5, 2.5)
+b = Intervalo(1, 3)
+c = Intervalo(BigFloat("0.1"), big(0.1))
+d = Intervalo(-1, 1)
+emptyFl = intervalo_vacio(Float64)
+emptyB = intervalo_vacio(Float64)
+
+@testset "Creación de intervalos" begin
+    @test typeof(a) == Intervalo{Float64}
+    @test getfield(a, :infimo) == 1.5
+    @test getfield(a, :supremo) == 2.5
+
+    @test typeof(b) == Intervalo{Float64}
+    @test getfield(b, :infimo) == 1.0
+    @test getfield(b, :supremo) == 3.0
+
+    @test typeof(c) == Intervalo{BigFloat}
+    @test getfield(c, :infimo) == BigFloat("0.1")
+    @test getfield(c, :supremo) == big(0.1)
+
+    @test isinf(getfield(emptyFl, :infimo))
+    @test isinf(getfield(emptyFl, :supremo))
+    @test signbit(getfield(emptyFl, :infimo)) == 0
+    @test signbit(getfield(emptyFl, :supremo)) == 1
+
+    @test isinf(getfield(emptyB, :infimo))
+    @test isinf(getfield(emptyB, :supremo))
+    @test getfield(emptyB, :infimo) == Inf
+    @test getfield(emptyB, :supremo) == -Inf
+
+    @test u == Intervalo(1.0)
+    @test z == Intervalo(0.0)
+    @test z == Intervalo(big(0.0))
+end
+
+@testset "Operaciones de conjuntos" begin
+    @test a == a
+    @test a !== b
+    @test c == c
+    @test emptyFl == emptyB
+    @test a ⊆ a
+    @test a ⊆ b
+    @test b ⊇ a
+    @test c ⊆ c
+    @test !(c ⊆ b) && !(b ⊆ c)
+    @test a ⪽ b
+    @test !(c ⪽ c)
+    @test a ∪ b == b
+    @test a ⊔ b == b
+    @test a ∪ emptyFl == a
+    @test c ⊔ emptyB == c
+    @test a ∩ b == a
+    @test a ∩ emptyFl == emptyFl
+    @test a ∩ c == emptyB
+    @test 0 ∈ z
+    @test 2 ∈ a
+    @test 3 ∉ a
+    @test 0.1 ∈ c
+end
+
+@testset "Operaciones aritméticas" begin
+    @test z + u == +u
+    @test z - u == -u
+    @test b + 1 == 1.0 + b == Intervalo(2, 4)
+    @test 2*(a - 2) == d
+    @test c - 0.1 !== z
+    @test 0.1 * u == Intervalo(0.1, 0.1)
+    @test d * 0.1 == Intervalo(-0.1, 0.1)
+    @test d * (a + b) ⊆ d*a + d*b
+    @test d^2 == Intervalo(0, 1)
+    @test d*d == d
+    @test d^3 == d
+    @test d^4 == d^2
+    @test u/a == inv(a)
+    @test 1 ∈ a/a
+    @test 1/a == inv(a)
+    @test (a-1)*(a-2) ⪽ (a*a -3*a + 3)
+    @test a/d == Intervalo(-Inf, Inf)
+    @test z/z == intervalo_vacio(z)
+end
