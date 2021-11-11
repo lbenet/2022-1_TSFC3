6d378ee6f221f75c44f07a6a090f908f20802170
Luis Benet <benet@icf.unam.mx>
2021-11-10 19:24:09 -0600

    Agrego Tarea2 y los tests respectivos

---
+++ tareas/Tarea2.jl
@@ -0,0 +1,31 @@
+# # Tarea 2
+
+#-
+# > **Fecha de entrega** (aceptación del *Pull Request*): Jueves 18 Noviembre
+#
+
+#-
+# ### Ejercicio 1: Redondeo
+#
+# Incluyan en la implementación del módulo `Intervalos` que hicieron para la Tarea 1
+# el redondeo usando `prevfloat` y `nextfloat`. (Esta implementación hará que los
+# tests anteriores ya no pasen).
+
+#-
+# ### Ejercicio 2: División extendida
+#
+# Incluyan una función, `division_extendida` que devuelva una *tupla de intervalos*
+# siguiendo la definición que vimos en la clase. En los casos que se requiera,
+# es necesario que su implementación incluya el redondeo.
+
+#-
+# ### Ejercicio 3: Tests
+#
+# Asegúrense que lo desarrollado en los Ejercicios 1 y 2 *pase* las pruebas que se encuentran en el archivo
+# `tareas/Tests/test2.jl`. Lo que deben hacer (desde el REPL) es:
+# ```julia
+#     include("tareas/SuDirTarea2/intervalos.jl")
+#     include("tareas/Tests/test2.jl")
+# ```
+# y esto debe **no** arrojar ningún error.
+#
