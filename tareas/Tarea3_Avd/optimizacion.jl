1e8285df4efe83b04bb04fd4c55f9479f9d4cebf
Avdrns <avdrns@ciencias.unam.mx>
2022-01-14 19:35:26 -0600

    Archivos limpiados y con los include como se piden

--- tareas/Tarea3_Avd/optimizacion.jl
+++ tareas/Tarea3_Avd/optimizacion.jl
@@ -1,70 +1,3 @@
-include("intervalos.jl")
-
-# ╔═╡ a7cc31d3-e34a-425a-a698-e4e869c57a05
-#using Plots   ### To help me visualize better what I'm trying and doing
-
-# ╔═╡ 7dd4495e-73fc-11ec-37a7-9fbab661cf88
-#=function mini(f, I, tol=1e-5)   ### Non-Interval minimize method
-	y = f(I.infimo)
-	x = I.infimo
-	for i in (I.infimo+tol):tol:I.supremo
-		if f(i-tol) > f(i) < f(i+tol)
-			if f(i) < y
-				y = f(i)
-				x = i
-			end
-		end
-	end
-	return y, x
-end=#
-
-# ╔═╡ aab40414-6df7-4f1b-8a86-5d216ff3b61f
-#=function maxi(f, I, tol=1e-5)   ### Non-Interval maximize method
-	y = f(I.infimo)
-	x = I.infimo
-	for i in (I.infimo+tol):tol:I.supremo
-		if f(i-tol) < f(i) > f(i+tol)
-			if f(i) > y
-				y = f(i)
-				x = i
-			end
-		end
-	end
-	return y, x
-end=#
-
-# ╔═╡ 6811f544-94ba-4998-9ab7-0c156f042736
-#=function minimiza_no_peaks(f, I, tol)
-	(I.supremo - I.infimo) < tol && return I
-	I₀ = I
-	while true
-		mp = I₀.infimo + (I₀.supremo - I₀.infimo)/2
-		I₁ = Intervalo(I₀.infimo, mp);   I₂ = Intervalo(mp, I₀.supremo)
-		Ints = [I₁, I₂];   ys = []
-		for i in Ints
-			step = (i.supremo - i.infimo)/100
-			y_min = Inf
-			for p in i.infimo:step:i.supremo
-				if f(p-step) > f(p) < f(p+tol)
-					if f(p) < y_min
-						y_min = f(p)
-					end
-				end
-			end
-			push!(ys, y_min)
-		end
-		if ys[1] < ys[2]
-			I₀ = Ints[1]
-		elseif ys[2] < ys[1]
-			I₀ = Ints[2]
-		end
-		if (I₀.supremo - I₀.infimo) < tol
-			return I₀
-		end
-	end 
-end=#
-
-# ╔═╡ 062f4a8e-0520-417f-b361-3f79de6c5a66
 """
 	subdividing(Is)
 Given a Intervalo's vector *Is*, for each element of it, subdivide them at half and return another Intervalo's vector with all the Intervalo's sub-intervals.
@@ -79,7 +12,10 @@ function subdividing(Is)
 	return subdiv
 end
 
-# ╔═╡ ddf1b388-e95d-4d8d-ad58-56ac884290e7
+"""
+	extrems(f, V)
+Calculate each extreme point of the function *f* for each Intervalo in *V*.
+"""
 function extrems(f, V)
 	l = length(V)
 	exts = zeros(l)
@@ -89,7 +25,6 @@ function extrems(f, V)
 	return exts
 end
 
-# ╔═╡ 5f4e4159-5517-4907-a583-3cdfbbcabbdb
 """
 	minimiza(f, I, tol)
 Find the interval(s) (type=Intervalo) which contains the minimum of the function *f* in the initial given interval (type=Intervalo) *I*, until the diameter of the answer(s) meets the condition to have less length than *tol*.
@@ -120,19 +55,6 @@ function minimiza(f, I::Intervalo, tol=1/1024)
 	end
 end
 
-# ╔═╡ bf68dde3-9148-4b3a-9261-de589885a749
-#=begin
-	g(x) = - x^3 + (x+2)^2 - 2x + 3
-	mn = minimiza(x->g(x), Intervalo(-2,3))
-	p = plot(-2:0.1:3, x->g(x), key=false)
-	for i in mn
-		plot!(p, [i.infimo, i.infimo, i.supremo, i.supremo, i.infimo],
-		[g(i).infimo, g(i).supremo, g(i).supremo, g(i).infimo, g(i).infimo], lw=2)
-	end
-	p
-end=#
-
-# ╔═╡ d3176cd6-4ff9-4ec9-8656-161de957557e
 """
 	maximiza(f, I, tol)
 Find the interval(s) (type=Intervalo) which contains the maximum of the function *f* in the initial given interval (type=Intervalo) *I*, until the diameter of the answer(s) meets the condition to have less length than *tol*.
@@ -162,23 +84,3 @@ function maximiza(f, I::Intervalo, tol=1/1024)
 		end
 	end
 end
-
-# ╔═╡ 960f4d88-d6bc-4a60-aa91-7743edf7453f
-#=begin
-	mx = maximiza(x->g(x), Intervalo(-1.4,3))
-	q = plot(-1.5:0.1:3, x->g(x), key=false)
-	for i in mx
-		plot!(q, [i.infimo, i.infimo, i.supremo, i.supremo, i.infimo],
-		[g(i).infimo, g(i).supremo, g(i).supremo, g(i).infimo, g(i).infimo], lw=2)
-	end
-	q
-end=#
-
-# ╔═╡ 4a4adcca-55fd-4866-b1fb-f38f1ba7820b
-#=begin
-	h(x) = 1 - x^4 + x^5
-	xm, ym = minimiza(h, Intervalo(0,1))
-	typeof(xm) <: Vector{T} where {T<: Intervalo}
-	any(4/5 .∈ xm)
-	all(ym .∈ h.(xm))
-end=#
--- tareas/Tarea3_Avd/raices.jl
+++ tareas/Tarea3_Avd/raices.jl
@@ -1,14 +1,13 @@
-using ForwardDiff#, Plots
+using ForwardDiff
 
-# ╔═╡ 1bd435bb-b9e8-40e1-83b9-6c65551c1280
-include("intervalos.jl")
-
-# ╔═╡ f6fbd260-bf9e-4ca9-9f7f-53100a24f962
 struct Raiz
 	raiz::Intervalo;   unicidad::Bool
 end
 
-# ╔═╡ 3305d0e0-4774-4555-9c1a-d1c6832985ae
+"""
+	INR_method(f, I, tol)
+Interval Newton-Raphson method.
+"""
 function INR_method(f, I, tol)
     0.0 ∉ f(I) && return Vector{Intervalo}()
 	roots = [I]
@@ -37,35 +36,10 @@ function INR_method(f, I, tol)
     end
 end
 
-# ╔═╡ 6bbea2d2-28f9-4d55-9ab9-e034a308e232
-#=function esmonotona_chida(f, I, part=1e-5)
-	interv = I.infimo:part:I.supremo
-	for i in interv
-		if ForwardDiff.derivative(f,i) == 0.0
-			return false
-		elseif ForwardDiff.derivative(f,i) < 0.0
-			for j in i:part:interv[end]
-				if ForwardDiff.derivative(f,j) == 0.0
-					return false
-				elseif ForwardDiff.derivative(f,j) > 0.0
-					return false
-				end
-			end
-			return true
-		elseif ForwardDiff.derivative(f,i) > 0.0
-			for k in i:part:interv[end]
-				if ForwardDiff.derivative(f,k) == 0.0
-					return false
-				elseif ForwardDiff.derivative(f,k) < 0.0
-					return false
-				end
-			end
-			return true
-		end
-	end
-end=#
-
-# ╔═╡ 3090f4f9-130d-4ec1-8689-fc9492893e2c
+"""
+	esmonotona(f, I)
+Check if the function *f* is monotonous in the Intervalo interval *I*.
+"""
 function esmonotona(f, I) 
 	if 0.0 ∈ ForwardDiff.derivative(f, I)
 		return false
@@ -74,48 +48,16 @@ function esmonotona(f, I)
 	end
 end
 
-# ╔═╡ ab894a96-c403-449c-b45c-b15eae978b1d
+"""
+unicity(f, V)
+Check if the roots of function *f* in the Intervalo interval *I* are unique or not.
+"""
 function unicity(f, V)
-	l = length(V);   uni = trues(l)
-	for I in 1:l
-		monot = esmonotona(f, V[I])
-		if monot == false
-			uni[I] = false
-		end
-	end
-	return uni
+	return esmonotona.(f, V)
 end
 
-# ╔═╡ 3d099169-4f12-4144-8073-898c7714f204
 function ceros_newton(f, I::Intervalo, tol=1/1024)
 	roots = INR_method(f, I, tol)
 	unis = unicity(f, roots)
 	return Raiz.(roots, unis)
 end
-
-# ╔═╡ ea5ee562-4ac7-4b09-975f-3a888e85db6e
-#=begin
-	h(x) = 4*x + 3
-	    #r = ceros_newton(h, Intervalo(-1,0), 0.25)
-	    #@test -0.75 ∈ getfield(r[1], :raiz)
-	    # Este ejemplo no tiene raices
-	    r = ceros_newton(h, Intervalo(1,2), 0.5)
-	    length(r) == 0
-end=#
-
-# ╔═╡ 49655b70-c745-48e6-a0dc-1aacc9bd8b2a
-#=begin
-	h3(x) = x^3*(4-5x)
-    rr3 = ceros_newton(h3, Intervalo(-big(3),4), big(1)/2^10)
-	INR_method(h3, Intervalo(-big(3),4), 0.09)
-    #all(0 .∈ h3.(getfield.(rr3, :raiz)))
-    #all(typeof.(getfield.(rr3, :raiz)) .== Intervalo{BigFloat})
-    #any( 4//5 .∈ getfield.(rr3, :raiz))
-	#getfield.(rr3, :raiz)
-    #any( 0 .∈ getfield.(rr3, :raiz))
-    #any(getfield.(rr3, :unicidad))
-    #!all(getfield.(rr3, :unicidad))
-end=#
-
-# ╔═╡ f6431349-8d03-4c8c-bd29-071ce4326275
-#plot(-0.1:0.01:0.1, x->h3(x), key=false)
